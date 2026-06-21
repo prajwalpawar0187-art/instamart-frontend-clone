cd04537a906e7882c966739d1c4fa47975b059e2
prajwalpawar0187-art <prajwalpawar0187@gmail.com>
2026-06-21 19:15:59 +0530

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,9 @@
+let playstore=document.querySelector(".playstore");
+playstore.onclick = () => {
+    window.location.href="https://play.google.com/store/apps/details?id=in.swiggy.android&hl=en-US&pli=1";
+}
+
+let applestore=document.querySelector(".applestore");
+applestore.onclick = () => {
+    window.location.href="https://apps.apple.com/in/app/swiggy-food-instamart-dineout/id989540920";
+}
